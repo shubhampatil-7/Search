330394878325f8905b7ef10d870aa7991422f93f
Shubham <patilshubham821@gmail.com>
2026-09-08 13:02:29 +0000

    semantic search

---
+++ cli/semantic_search_cli.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    args = parser.parse_args()
+
+    match args.command:
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
